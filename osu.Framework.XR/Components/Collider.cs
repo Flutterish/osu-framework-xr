08c4112b8715417e3f04e3c5ebb6612fd91cd3d1
Flutterish <CharmindYT@gmail.com>
2021-03-21 18:19:30 +0100

    moved physics from oxr

---
+++ osu.Framework.XR/Components/Collider.cs
@@ -0,0 +1,7 @@
+﻿using osu.Framework.XR.Physics;
+
+namespace osu.Framework.XR.Components {
+	public class Collider : Model, IHasCollider {
+		public bool IsColliderEnabled { get; set; } = true;
+	}
+}
---
+++ osu.Framework.XR/Physics/IHasCollider.cs
@@ -0,0 +1,8 @@
+﻿using osu.Framework.XR.Graphics;
+
+namespace osu.Framework.XR.Physics {
+	public interface IHasCollider {
+		Mesh Mesh { get; }
+		bool IsColliderEnabled { get; }
+	}
+}
