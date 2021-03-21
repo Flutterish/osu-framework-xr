ddcc7e1b10b94d8ae64df9ea7dc316f32fb1175a
Flutterish <CharmindYT@gmail.com>
2021-03-21 19:42:27 +0100

    Create BindableSet.cs

---
+++ osu.Framework.XR/BindableSet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.Framework.XR {
+	public class BindableSet<T> : IEnumerable<T> {
+		private HashSet<T> set = new();
+
+		public event Action<T> ItemAdded;
+		public event Action<T> ItemRemoved;
+		public event Action CollectionChanged;
+
+		public void BindCollectionChanged ( Action action, bool runOnceImmediately = false ) {
+			CollectionChanged += action;
+			if ( runOnceImmediately ) action?.Invoke();
+		}
+
+		public void Add ( T value ) {
+			if ( set.Contains( value ) ) return;
+
+			set.Add( value );
+			ItemAdded?.Invoke( value );
+			CollectionChanged?.Invoke();
+		}
+
+		public void Remove ( T value ) {
+			if ( !set.Contains( value ) ) return;
+
+			set.Remove( value );
+			ItemRemoved?.Invoke( value );
+			CollectionChanged?.Invoke();
+		}
+
+		public IEnumerator<T> GetEnumerator () {
+			return ( (IEnumerable<T>)set ).GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator () {
+			return ( (IEnumerable)set ).GetEnumerator();
+		}
+	}
+}
