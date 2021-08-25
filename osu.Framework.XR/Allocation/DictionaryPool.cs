e1dcf914d603308b8dd2b7acaf9e1f8ba3649395
Flutterish <CharmindYT@gmail.com>
2021-08-25 22:35:52 +0200

    implement object pools with `using` semantics to reduce allocations

---
+++ osu.Framework.XR/Allocation/DictionaryPool.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using System;
+using System.Collections.Generic;
+
+namespace osu.Framework.XR.Allocation {
+	public class DictionaryPool<K,V> where K : notnull {
+		private DictionaryPool () {
+			pool = ObjectPool.Create( new PoolPolicy( this ) );
+		}
+
+		private ObjectPool<PooledDictionary<K,V>> pool;
+
+		public static PooledDictionary<K, V> Shared { get; } = new();
+
+		public PooledDictionary<K, V> Rent ()
+			=> pool.Get();
+
+		public void Return ( PooledDictionary<K, V> list )
+			=> pool.Return( list );
+
+		private class PoolPolicy : IPooledObjectPolicy<PooledDictionary<K,V>> {
+			private DictionaryPool<K,V> parent;
+			public PoolPolicy ( DictionaryPool<K,V> parent ) {
+				this.parent = parent;
+			}
+
+			public PooledDictionary<K,V> Create ()
+				=> new( parent );
+
+			public bool Return ( PooledDictionary<K, V> obj ) {
+				obj.Clear();
+				return true;
+			}
+		}
+	}
+
+	public class PooledDictionary<K,V> : Dictionary<K,V>, IDisposable where K : notnull {
+		private DictionaryPool<K,V> pool;
+		public PooledDictionary () {
+			throw new InvalidOperationException( "This constructor should never be called." );
+		}
+
+		public PooledDictionary ( DictionaryPool<K, V> pool ) {
+			this.pool = pool;
+		}
+
+		public void Dispose () {
+			pool.Return( this );
+		}
+	}
+}
---
+++ osu.Framework.XR/Allocation/ListPool.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using System;
+using System.Collections.Generic;
+
+namespace osu.Framework.XR.Allocation {
+	public class ListPool<T> {
+		private ListPool () {
+			pool = ObjectPool.Create( new PoolPolicy( this ) );
+		}
+
+		private ObjectPool<PooledList<T>> pool;
+
+		public static ListPool<T> Shared { get; } = new();
+
+		public PooledList<T> Rent ()
+			=> pool.Get();
+
+		public void Return ( PooledList<T> list )
+			=> pool.Return( list );
+
+		private class PoolPolicy : IPooledObjectPolicy<PooledList<T>> {
+			private ListPool<T> parent;
+			public PoolPolicy ( ListPool<T> parent ) {
+				this.parent = parent;
+			}
+
+			public PooledList<T> Create ()
+				=> new( parent );
+
+			public bool Return ( PooledList<T> obj ) {
+				obj.Clear();
+				return true;
+			}
+		}
+	}
+
+	public class PooledList<T> : List<T>, IDisposable {
+		private ListPool<T> pool;
+		public PooledList () {
+			throw new InvalidOperationException( "This constructor should never be called." );
+		}
+
+		public PooledList ( ListPool<T> pool ) {
+			this.pool = pool;
+		}
+
+		public void Dispose () {
+			pool.Return( this );
+		}
+	}
+}
