6d10e82108ae09de38354b7b616d06d2bb2fbb9e
Flutterish <CharmindYT@gmail.com>
2021-08-25 23:28:12 +0200

    Create StackPool.cs

---
+++ osu.Framework.XR/Allocation/StackPool.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using System;
+using System.Collections.Generic;
+
+namespace osu.Framework.XR.Allocation {
+	public class StackPool<T> {
+		private StackPool () {
+			pool = ObjectPool.Create( new PoolPolicy( this ) );
+		}
+
+		private ObjectPool<PooledStack<T>> pool;
+
+		public static StackPool<T> Shared { get; } = new();
+
+		public PooledStack<T> Rent ()
+			=> pool.Get();
+
+		public PooledStack<T> Rent ( T content ) {
+			var list = pool.Get();
+			list.Push( content );
+			return list;
+		}
+
+		public void Return ( PooledStack<T> list )
+			=> pool.Return( list );
+
+		private class PoolPolicy : IPooledObjectPolicy<PooledStack<T>> {
+			private StackPool<T> parent;
+			public PoolPolicy ( StackPool<T> parent ) {
+				this.parent = parent;
+			}
+
+			public PooledStack<T> Create ()
+				=> new( parent );
+
+			public bool Return ( PooledStack<T> obj ) {
+				obj.Clear();
+				return true;
+			}
+		}
+	}
+
+	public class PooledStack<T> : Stack<T>, IDisposable {
+		private StackPool<T> pool;
+		public PooledStack () {
+			throw new InvalidOperationException( "This constructor should never be called." );
+		}
+
+		public PooledStack ( StackPool<T> pool ) {
+			this.pool = pool;
+		}
+
+		public void Dispose () {
+			pool.Return( this );
+		}
+	}
+}
