60802f167cd59e945d94657d07de4dc702cd9c4b
Flutterish <CharmindYT@gmail.com>
2022-09-29 19:55:07 +0200

    remove more members unused by 3d drawables

--- osu.Framework.XRv2/Graphics/Drawable3D.UnusedMembers.cs
+++ osu.Framework.XRv2/Graphics/Drawable3D.UnusedMembers.cs
@@ -3,15 +3,21 @@ using osu.Framework.Graphics.Primitives;
 
 namespace osu.Framework.XR.Graphics;
 public partial class Drawable3D {
-	public sealed override bool UpdateSubTreeMasking ( Drawable source, RectangleF maskingBounds ) {
-		return false;
-	}
-
-	protected override void UpdateAfterAutoSize () { }
-	protected override bool ReceivePositionalInputAtSubTree ( Vector2 screenSpacePos )
+	public sealed override bool UpdateSubTreeMasking ( Drawable source, RectangleF maskingBounds )
 		=> false;
 
-	public override bool ReceivePositionalInputAt ( Vector2 screenSpacePos )
+	public sealed override DrawColourInfo DrawColourInfo => default;
+	public sealed override DrawInfo DrawInfo => default;
+	protected sealed override RectangleF ComputeChildMaskingBounds ( RectangleF maskingBounds )
+		=> default;
+
+	public sealed override Quad ScreenSpaceDrawQuad => default;
+
+	protected sealed override void UpdateAfterAutoSize () { }
+	protected sealed override bool ReceivePositionalInputAtSubTree ( Vector2 screenSpacePos )
+		=> false;
+
+	public sealed override bool ReceivePositionalInputAt ( Vector2 screenSpacePos )
 		=> false;
 
 	public sealed override bool PropagatePositionalInputSubTree => false;
@@ -21,6 +27,6 @@ public partial class Drawable3D {
 
 	public sealed override bool HandlePositionalInput => false;
 
-	public override bool Contains ( Vector2 screenSpacePos )
+	public sealed override bool Contains ( Vector2 screenSpacePos )
 		=> false;
 }
