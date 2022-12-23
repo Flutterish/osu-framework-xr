61e6244d40a75f5af02064c19b42d79420df5814
Flutterish <CharmindYT@gmail.com>
2022-12-23 05:38:29 +0100

    add sum/average for vector3 enumerables

---
+++ osu.Framework.XR/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace osu.Framework.XR;
+
+public static class EnumerableExtensions {
+	public static Vector3 Average ( this IEnumerable<Vector3> vectors ) {
+		int count = 0;
+		Vector3 sum = Vector3.Zero;
+		foreach ( var i in vectors ) {
+			count++;
+			sum += i;
+		}
+
+		return sum / count;
+	}
+
+	public static Vector3 Sum ( this IEnumerable<Vector3> vectors ) {
+		Vector3 sum = Vector3.Zero;
+		foreach ( var i in vectors ) {
+			sum += i;
+		}
+
+		return sum;
+	}
+}
